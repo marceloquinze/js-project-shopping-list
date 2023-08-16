fae039406b47680448277f58ca759f0639ebed4f
Marcelo Vieira <marceloquinze@gmail.com>
2023-08-16 19:21:22 -0300

    Added filter function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.querySelector('#item-form');
 const itemInput = document.querySelector('#item-input');
 const itemList = document.querySelector('#item-list');
 const clearBtn = document.querySelector('#clear');
+const itemFilter = document.querySelector('.filter');
 
 const addItem = (e) => {
 	e.preventDefault();
@@ -26,6 +27,8 @@ const addItem = (e) => {
 	button.appendChild(icon);
 	li.appendChild(button);
 	itemList.appendChild(li);
+	// Check the UI as soon as we create the <li>s
+	resetState();
 
 	// Clear input after creating elements
 	itemInput.value = '';
@@ -48,7 +51,10 @@ const removeItem = (e) => {
 	// Only remove if we click on an element whose parent has a certain class
 	if (e.target.parentElement.classList.contains('remove-item')) {
 		// Traverse the DOM and remove the right element: <li>
-		e.target.parentElement.parentElement.remove();
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+			resetState();
+		}
 	}
 };
 
@@ -57,9 +63,42 @@ const clearItems = () => {
 	while (itemList.firstChild) {
 		itemList.removeChild(itemList.firstChild);
 	}
+	resetState();
+};
+
+const filterItems = (e) => {
+	const items = itemList.querySelectorAll('li');
+	const text = e.target.value.toLowerCase();
+
+	items.forEach((item) => {
+		const itemName = item.firstChild.textContent.toLowerCase();
+
+		if (itemName.indexOf(text) != -1) {
+			item.style.display = 'flex';
+		} else {
+			item.style.display = 'none';
+		}
+	});
+};
+
+const resetState = () => {
+	// We need to define the <li>s here, upon function call
+	const items = itemList.querySelectorAll('li');
+	if (items.length === 0) {
+		clearBtn.style.display = 'none';
+		itemFilter.style.display = 'none';
+	} else {
+		clearBtn.style.display = 'block';
+		itemFilter.style.display = 'block';
+	}
 };
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+// Check if there are items to clear filter and clear button
+// Global Scope
+resetState();
